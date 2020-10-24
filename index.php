6d6c7be85f4fc34443d0e71a0909b560ed85a821
einnabno <70646362+einnabno@users.noreply.github.com>
2020-10-24 22:16:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+ <html lang="em">
+ <meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-compatible" content="ei-edge">
+	<title> SVG OBJECT </title>
+	<link rel="stylesheet" href="css/style.css">
+	
+	
+<svg class="s1">
+  <ellipse stroke="#000" ry="26.25043" rx="27.00044" id="svg_40" cy="174.47697" cx="133.24971" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" fill="#972912"/>
+  <ellipse ry="15.75026" rx="18.75031" id="svg_38" cy="183.47711" cx="139.49981" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#fff"/>
+  <ellipse transform="rotate(6.404417514801025 222.75117492675832,173.47694396972656) " stroke="#000" ry="26.25043" rx="27.00044" id="svg_41" cy="173.47695" cx="222.75118" stroke-width="1.5" fill="#972912"/>
+  <ellipse ry="15.75026" rx="18.75031" id="svg_39" cy="183.47711" cx="218.50111" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#fff"/>
+  <ellipse stroke="#000" ry="42.00069" rx="45.00074" id="svg_37" cy="223.72777" cx="180.75049" stroke-width="1.5" fill="#EF914E"/>
+  <ellipse stroke="#000" ry="3.25005" rx="8.25014" id="svg_43" cy="253.97826" cx="182.00051" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" fill="#000"/>
+  <ellipse ry="9.50016" rx="10.75018" id="svg_44" cy="215.72764" cx="164.50022" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#fff"/>
+  <ellipse ry="9.50016" rx="10.75018" id="svg_45" cy="215.72764" cx="196.50074" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#fff"/>
+  <ellipse stroke="#000" ry="4.50007" rx="5.00008" id="svg_46" cy="219.7277" cx="166.75026" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" fill="#000"/>
+  <ellipse stroke="#000" ry="4.75008" rx="5.75009" id="svg_47" cy="219.97771" cx="197.00075" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" fill="#000"/>
+<svg class="s2">
+  <line stroke-linecap="null" stroke-linejoin="null" id="svg_2" y2="251.45313" x2="121.5" y1="245.45313" x1="149.5" stroke-width="1.5" stroke="#000" fill="none"/>
+  <line transform="rotate(-14.876288414001465 138.83328247070327,254.28637695312506) " stroke-linecap="null" stroke-linejoin="null" id="svg_5" y2="257.28637" x2="124.83328" y1="251.28637" x1="152.83328" stroke-width="1.5" stroke="#000" fill="none"/>
+  <line transform="rotate(25.4588680267334 227.16532897949207,246.45315551757815) " stroke-linecap="null" stroke-linejoin="null" id="svg_6" y2="249.45315" x2="213.16533" y1="243.45315" x1="241.16533" stroke-width="1.5" stroke="#000" fill="none"/>
+  <line transform="rotate(42.8333625793457 221.66532897949227,254.45306396484372) " stroke-linecap="null" stroke-linejoin="null" id="svg_7" y2="257.45306" x2="207.66534" y1="251.45306" x1="235.66534" stroke-width="1.5" stroke="#000" fill="none"/>
+</svg>
+</svg>
